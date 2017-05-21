27ea3a22ce24c2b8ac87f1936c0d3db829e69078
kvin1087 <kaustubh87@gmail.com>
2017-05-21 12:39:19 -0500

    Added  service for the templates controller

--- Web-Template-Store/app/templates/templates.js
+++ Web-Template-Store/app/templates/templates.js
@@ -11,8 +11,11 @@ angular.module('templateStore.templates',['ngRoute'])
     });
 }])
 
-.controller('TemplatesCtrl', ['$scope', function($scope){
+.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
     
+    $http.get('json/templates.json').success(function(data){
+        $scope.templates = data;
+    });
 }])
 
 .controller('TemplateDetailsCtrl', ['$scope', function($scope){
