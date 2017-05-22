ddcc31c7130529f89d6f6eb0c0b9eff5c8c4cf79
kvin1087 <kaustubh87@gmail.com>
2017-05-21 20:38:13 -0500

    Added details ControlleR

--- Web-Template-Store/app/templates/templates.js
+++ Web-Template-Store/app/templates/templates.js
@@ -18,7 +18,15 @@ angular.module('templateStore.templates',['ngRoute'])
     });
 }])
 
-.controller('TemplateDetailsCtrl', ['$scope', function($scope){
-    console.log($scope);
+.controller('TemplateDetailsCtrl', ['$scope','$http', '$routeParams','$filter',function($scope,$http,$routeParams,$filter){
+    
+    var templateId = $routeParams.templateId;
+    $http.get('json/templates.json').success(function(data){
+        $scope.template = $filter('filter')(data, function(d){
+            return d.id == templateId;
+        })[0];
+        $scope.mainImage = $scope.template.images[0].name;
+    });
+    
     
 }])
